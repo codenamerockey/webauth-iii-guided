c4ae7093947bb652e3ef8f09c8c09e6601484792
Rockey Simmons <codenamerockey@gmail.com>
2019-10-16 09:53:47 -0700

    added jwt library and token to login http method

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,8 +25,10 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +38,16 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    subject: user.id
+  };
+  const secret = 'Shhhh.... this is a secret';
+  const options = {
+    expiresIn: '1h'
+  };
+}
 
 module.exports = router;
